37b8d78eaaa22ed9fbee81dd80d77359db85648a
eathyn <eathynzakas@gmail.com>
2025-12-12 22:15:20 +0800

    style: 🎨 更正代码风格

--- src/App.vue
+++ src/App.vue
@@ -42,10 +42,10 @@ const assemblySize = computed(() => globalStore.assemblySize);
 // element button config
 const buttonConfig = reactive({ autoInsertSpace: false });
 
-console.log('invalid commit message')
-console.log('invalid commit message 2')
-console.log('invalid commit message 3')
-console.log('invalid commit message 4')
-console.log('invalid commit message 5')
-console.log('测试仓库能否拒绝不符合规范的 Commit')
+console.log("invalid commit message");
+console.log("invalid commit message 2");
+console.log("invalid commit message 3");
+console.log("invalid commit message 4");
+console.log("invalid commit message 5");
+console.log("测试仓库能否拒绝不符合规范的 Commit");
 </script>
