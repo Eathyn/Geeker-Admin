4da26ae544ed7a036a79311bfe4cc4ac5c946f4a
eathyn <eathynzakas@gmail.com>
2025-12-16 17:13:38 +0800

    build: 📦️ 修改一个文件导致大部分文件的文件哈希都改变
    TODO: 分包似乎可以彻底解决这个问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import { resolve } from "path";
 import { wrapperEnv } from "./build/getEnv";
 import { createProxy } from "./build/proxy";
 import { createVitePlugins } from "./build/plugins";
-import pkg from "./package.json";
-import dayjs from "dayjs";
+// import pkg from "./package.json";
+// import dayjs from "dayjs";
 
-const { dependencies, devDependencies, name, version } = pkg;
-const __APP_INFO__ = {
-  pkg: { dependencies, devDependencies, name, version },
-  lastBuildTime: dayjs().format("YYYY-MM-DD HH:mm:ss")
-};
+// const { dependencies, devDependencies, name, version } = pkg;
+// const __APP_INFO__ = {
+//   pkg: { dependencies, devDependencies, name, version },
+//   lastBuildTime: dayjs().format("YYYY-MM-DD HH:mm:ss")
+// };
 
 // @see: https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
@@ -27,9 +27,9 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
         "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js"
       }
     },
-    define: {
-      __APP_INFO__: JSON.stringify(__APP_INFO__)
-    },
+    // define: {
+    //   __APP_INFO__: JSON.stringify(__APP_INFO__)
+    // },
     css: {
       preprocessorOptions: {
         scss: {
